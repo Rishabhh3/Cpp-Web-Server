ca7eb33397d42a028f5cd7c0606a96e4f9758f8b
Rishabhh3 <rishabhchamoli0120@gmail.com>
2025-05-27 08:59:53 +0530

    Add files via upload

---
+++ SimpleSocket.cpp
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+#include<SimpleSocket.hpp>
+
+//DEFAULT CONSTRUCTOR
+
+HDE::SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port, u_long interface)
+{
+    // DEFINE ADDRESS STRUCTURE
+    address.sin_family = domain;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = htonl(interface);
+    // interface is my IP adress
+
+    //ESTABLISH SOCKET
+    sock = socket(domain, service, protocol);
+    test_connection(sock); // test socket
+
+    //ESTABLISH NETWORK CONNECTION
+    connection = connect_to_network(sock, address);
+    test_connection(connection);
+
+    // BIND
+    //bind is only in server side code on the
+    //client side it is replaced with connect
+
+}
+
+// TEST CONNECTION VIRTUAL FUNCITON
+
+void HDE::SimpleSocket::test_connection(int item_to_test)
+{
+    // Confirms that the socket or connection  has been properly established
+    if(item_to_test < 0){
+        perror("Failed to Connect...");
+        exit(EXIT_FAILURE);
+    }
+}
+
+//Getter functions
+
+struct sockaddr_in HDE::SimpleSocket::get_address()
+{
+    return address;
+}
+
+int HDE::SimpleSocket::getsock()
+{
+    return sock;
+}
+
+int HDE::SimpleSocket::getconnection()
+{
+    return connection;
+}
